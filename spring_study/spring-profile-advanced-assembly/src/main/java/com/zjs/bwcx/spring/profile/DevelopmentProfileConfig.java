902907d54155bcd7ef2ac26ca7b2af6b866c990c
shizhijie <18232529363@163.com>
2019-01-14 17:47:53 +0800

    profile advanced assembly

---
+++ spring_study/spring-profile-advanced-assembly/src/main/java/com/zjs/bwcx/spring/profile/DevelopmentProfileConfig.java
@@ -0,0 +1,24 @@
+package com.zjs.bwcx.spring.profile;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@Profile("dev")
+public class DevelopmentProfileConfig {
+	
+	@Bean(destroyMethod="shutdown")
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.H2)
+				.addScript("classpath:schema.sql")
+				.addScript("classpath:test-data.sql")
+				.build();
+	}
+	
+}
