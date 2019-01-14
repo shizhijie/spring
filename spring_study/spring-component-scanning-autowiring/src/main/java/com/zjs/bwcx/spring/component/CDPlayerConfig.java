3a2c782722bcfc95b3958b3d54affcef5cd493bc
shizhijie <18232529363@163.com>
2019-01-14 12:20:11 +0800

    # WARNING: head commit changed in the meantime
    bwcx

--- spring_study/spring-component-scanning-autowiring/src/main/java/com/zjs/bwcx/spring/component/CDPlayerConfig.java
+++ spring_study/spring-component-scanning-autowiring/src/main/java/com/zjs/bwcx/spring/component/CDPlayerConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages= {"com.zjs.bwcx.spring.component"})
 public class CDPlayerConfig {
 }
