250fb0faa0f7a7cf09d09b636eae2d5a7385a1d0
shizhijie <18232529363@163.com>
2019-01-15 16:54:44 +0800

    spring-scope-beans

---
+++ spring_study/spring-scope-beans/src/main/java/com/zjs/bwcx/spring/scope/session/ShoppingCart.java
@@ -0,0 +1,19 @@
+package com.zjs.bwcx.spring.scope.session;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+
+@Component
+@Scope(
+		value = WebApplicationContext.SCOPE_SESSION,
+		proxyMode = ScopedProxyMode.INTERFACES)
+public class ShoppingCart {
+	
+	
+	public ShoppingCart cart() {
+		return null;
+	}
+	
+}
