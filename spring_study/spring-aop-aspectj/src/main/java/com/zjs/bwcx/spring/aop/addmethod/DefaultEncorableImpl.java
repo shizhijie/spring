6d7e95f5299e22fe1d2e61d8e6615f4109c67e61
shizhijie <18232529363@163.com>
2019-01-17 21:10:35 +0800

    DefaultEncorableImpl.java

--- spring_study/spring-aop-aspectj/src/main/java/com/zjs/bwcx/spring/aop/addmethod/DefaultEncorableImpl.java
+++ spring_study/spring-aop-aspectj/src/main/java/com/zjs/bwcx/spring/aop/addmethod/DefaultEncorableImpl.java
@@ -2,7 +2,6 @@ package com.zjs.bwcx.spring.aop.addmethod;
 
 public class DefaultEncorableImpl implements Encorable{
 
-	@Override
 	public void performEncore() {
 		System.out.println("hello 我是为Performance所有子类添加的新方法  请大家多多指教！！！");
 	}
