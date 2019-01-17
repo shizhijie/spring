addb8b0edd69ef262b966309d4774aac0d4ca3dd
shizhijie <18232529363@163.com>
2019-01-17 11:38:33 +0800

    com.zjs.bwcx.spring.aop.allbasedxmlpointcutextract

---
+++ spring_study/spring-aop-aspectj/src/main/java/com/zjs/bwcx/spring/aop/allbasedxmlpointcutextract/Audience.java
@@ -0,0 +1,30 @@
+package com.zjs.bwcx.spring.aop.allbasedxmlpointcutextract;
+
+import org.aspectj.lang.annotation.AfterReturning;
+
+public class Audience {
+	
+	public void performance() {}
+	
+	public void silenceCellPhones() {//表演之前 手机静音
+		System.out.println("Silencing cell phones");
+	}
+	
+	public void taskSeats() {
+		System.out.println("Tasking seats");
+	}
+	
+	@AfterReturning("performance()")
+	public void applause() {//表演成功 喝彩鼓掌
+		System.out.println("CLAP CLAP CLAP!!!");
+	}
+	
+	public void demandRefund() {// 表演失败之后 要求退款
+		System.out.println("Demanding a refund");
+	}
+	
+	public void end() {//表演成功 喝彩鼓掌
+		System.out.println("i am after");
+	}
+	
+}
---
+++ spring_study/spring-aop-aspectj/src/main/java/com/zjs/bwcx/spring/aop/allbasedxmlpointcutextract/Performance.java
@@ -0,0 +1,5 @@
+package com.zjs.bwcx.spring.aop.allbasedxmlpointcutextract;
+
+public interface Performance {
+	public void perform();
+}
