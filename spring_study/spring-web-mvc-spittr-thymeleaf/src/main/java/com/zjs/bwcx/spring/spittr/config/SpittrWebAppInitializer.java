21ab1bd88ccb6128f7dba403fb5a3591b91bdf35
shizhijie <18232529363@163.com>
2019-01-22 18:37:11 +0800

    spring-web-mvc-spittr-thymeleaf

---
+++ spring_study/spring-web-mvc-spittr-thymeleaf/src/main/java/com/zjs/bwcx/spring/spittr/config/SpittrWebAppInitializer.java
@@ -0,0 +1,23 @@
+package com.zjs.bwcx.spring.spittr.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		//将DispatcherServlet映射到 "/"
+		return new String[] { "/" };
+	}
+
+}
