206c17aeb04c486feb6246b39b048eec8695f4eb
shizhijie <18232529363@163.com>
2019-01-23 10:57:09 +0800

    spring-web-mvc-spittr-thymeleaf

--- spring_study/spring-web-mvc-spittr-thymeleaf/src/main/java/com/zjs/bwcx/spring/spittr/config/RootConfig.java
+++ spring_study/spring-web-mvc-spittr-thymeleaf/src/main/java/com/zjs/bwcx/spring/spittr/config/RootConfig.java
@@ -1,7 +1,10 @@
 package com.zjs.bwcx.spring.spittr.config;
 
+import java.util.regex.Pattern;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,5 +13,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = { "com.zjs.bwcx.spring.spittr" }, excludeFilters = {
 		@Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class) })
 public class RootConfig {
-
+	public static class WebPackage extends RegexPatternTypeFilter {
+	    public WebPackage() {
+	      super(Pattern.compile("com.zjs.bwcx.spring.spittr\\.web"));
+	    }    
+	  }
 }
--- spring_study/spring-web-mvc-spittr-thymeleaf/src/main/java/com/zjs/bwcx/spring/spittr/config/WebConfig.java
+++ spring_study/spring-web-mvc-spittr-thymeleaf/src/main/java/com/zjs/bwcx/spring/spittr/config/WebConfig.java
@@ -1,42 +1,41 @@
 package com.zjs.bwcx.spring.spittr.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.TemplateResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("com.zjs.bwcx.spring.spittr.web") //启用组件扫描
-public class WebConfig extends /*WebMvcConfigurerAdapter过时5.0*/ WebMvcConfigurationSupport{
-	
-	@Bean
-	public TemplateResolver templateReslover() {//模板解析器
-		TemplateResolver templateResolver = new TemplateResolver();
-		templateResolver.setPrefix("/WEB-INF/templates");
+@ComponentScan(basePackages= {"com.zjs.bwcx.spring.spittr.web","com.zjs.bwcx.spring.spittr.config"}) // 启用组件扫描
+public class WebConfig extends /* WebMvcConfigurerAdapter过时5.0 */ WebMvcConfigurationSupport {
+
+	/*@Bean
+	public TemplateResolver templateReslover() {// 模板解析器
+		TemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/views");
 		templateResolver.setSuffix(".html");
 		templateResolver.setTemplateMode("HTML5");
 		return templateResolver;
 	}
-	
+
 	@Bean
-	public TemplateEngine templateEngine(TemplateResolver templateReslover) {//模板引擎
+	public TemplateEngine templateEngine(TemplateResolver templateReslover) {// 模板引擎
 		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
 		templateEngine.setTemplateResolver(templateReslover);
 		return templateEngine;
 	}
-	
-	//Thymeleaf视图解析器
+
+	// Thymeleaf视图解析器
 	@Bean
 	public ViewResolver viewResolver(SpringTemplateEngine springTemplateEngine) {
 		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
 		viewResolver.setTemplateEngine(springTemplateEngine);
 		return viewResolver;
 	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}*/
 }
