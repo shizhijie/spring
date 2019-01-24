062b7ce7d81fb37d4a905e06d940028eb42594a6
shizhijie <18232529363@163.com>
2019-01-24 22:26:22 +0800

    spring-web-mvc-spittr-multipart

--- spring_study/spring-web-mvc-spittr-multipart/src/main/java/com/zjs/bwcx/spring/spittr/config/ThymeleafConfig.java
+++ spring_study/spring-web-mvc-spittr-multipart/src/main/java/com/zjs/bwcx/spring/spittr/config/ThymeleafConfig.java
@@ -15,6 +15,8 @@ public class ThymeleafConfig {
 	public ViewResolver viewResolver(org.thymeleaf.spring4.SpringTemplateEngine templateEngine) {
 		org.thymeleaf.spring4.view.ThymeleafViewResolver viewResolver = new org.thymeleaf.spring4.view.ThymeleafViewResolver();
 		viewResolver.setTemplateEngine(templateEngine);
+		viewResolver.setOrder(1);
+		viewResolver.setCharacterEncoding("utf-8");
 		return viewResolver;
 	}
 
@@ -31,6 +33,8 @@ public class ThymeleafConfig {
 		templateResolver.setPrefix("/WEB-INF/views/");
 		templateResolver.setSuffix(".html");
 		templateResolver.setTemplateMode("HTML5");
+		templateResolver.setCharacterEncoding("utf-8");
+		templateResolver.setCacheable(false);
 		return templateResolver;
 	}
 }
